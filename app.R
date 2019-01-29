ab8802b6acf4a41413da966dcca5af632d1a44a3
Sylvia Lee <sylvialee19@hotmail.com>
2019-01-28 16:30:07 -0800

    converted ggplot to plotly

--- app.R
+++ app.R
@@ -6,6 +6,7 @@ library(RColorBrewer)
 library(gridExtra)
 library(forcats)
 library(shinyhelper)
+library(plotly)
 
 # Load Data
 crime_data <- read.csv('data//ucr_crime_1975_2015_Final_Clean.csv',stringsAsFactors = FALSE)
@@ -42,7 +43,10 @@ ui <- fluidPage(
 
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("graphs"),
+      fluidRow(
+        column(width = 6, plotlyOutput("occurance_plot")),
+        column(width = 6, plotlyOutput("ratio_plot"))
+      ),
       tableOutput('State_Crime_Data'))
 
   )
@@ -61,13 +65,13 @@ server <- function(input, output) {
       filter(year >= input$yearInput[1] & year <= input$yearInput[2]) %>%
       filter(department_name==input$Selected_Department) %>%
       filter(crime_type%in%input$crimeTypeInput) %>%
-      { if(input$countMeasure == "Total Counts") select(.,c('year',
+      { if(input$countMeasure == "Total Counts") select(.,c(Year = 'year',
                                                             'US_State',
                                                             'department_name',
                                                             'total_crime_count',
                                                             'crime_type',
                                                             'counts'))
-        else select(., c('year',
+        else select(., c(Year = 'year',
                         'US_State',
                         'department_name',
                          total_crime_count='total_crime_per_100k',
@@ -87,7 +91,10 @@ server <- function(input, output) {
   # Create plots
   occurance_lineplot <- reactive({
     crime_data_selected () %>%
-      ggplot(aes(year,counts, group = interaction(department_name, crime_type), color = crime_type))+
+      ggplot(aes(Year,counts, group = interaction(department_name, crime_type), color = crime_type,
+             text = paste("Year:", Year,
+                          "<BR>Crime Counts:", counts,
+                          sep = "")))+
       geom_line(size = 1.25)+
       geom_point(size = 2)+
       labs(x = "Year",
@@ -95,13 +102,19 @@ server <- function(input, output) {
            title = "Number of crimes")+
       scale_color_manual(values = color_map)+
       theme_minimal()+
-      theme(legend.position = "none",plot.title = element_text(size=20))
+      theme(legend.position = "none")
   })
+  
+  occurance_lineplotly <- reactive({
+    ggplotly(occurance_lineplot(), tooltip = "text")})
 
 
   ratio_plot <- reactive({
     crime_data_selected () %>%
-      ggplot(aes(as.factor(year),count_ratio, fill=crime_type))+
+      ggplot(aes(as.factor(Year),count_ratio, fill=crime_type,
+                 text = paste("Year:", Year,
+                              "<BR>Crime Proportion:", round(count_ratio,3),
+                              sep = "")))+
       geom_bar(stat = 'identity')+
       labs(x = "Year",
            y = "Proportion Of Crimes",
@@ -110,17 +123,22 @@ server <- function(input, output) {
       scale_fill_manual(values = color_map)+
       theme_minimal()+
       theme(axis.text.x = element_text(angle = 70, vjust = 0.5),
-            legend.position = "bottom", plot.title = element_text(size=20))
+            legend.position = "bottom")
   })
-
+  
+  ratio_plotly <- reactive({
+    ggplotly(ratio_plot(), tooltip = "text") %>% 
+      layout(legend = list(orientation = "h", x = -0.25, y = -0.2))
+  })
+  
   # Calculate summary stats
   summary_table <- reactive({
     crime_data_selected() %>%
-      filter(year == max(year) | year == min(year)) %>%
+      filter(Year == max(Year) | Year == min(Year)) %>%
       group_by(crime_type) %>%
       mutate(net_diff = counts - lag(counts),
              perc_diff = (count_ratio - lag(count_ratio))*100,
-             time_diff = year-lag(year)) %>%
+             time_diff = Year-lag(Year)) %>%
       mutate(avg_annual_diff = net_diff/time_diff,
              avg_annual_perc_diff = perc_diff/time_diff) %>%
       select(crime_type, net_diff, perc_diff, avg_annual_diff, avg_annual_perc_diff) %>%
@@ -144,8 +162,11 @@ server <- function(input, output) {
                      label = 'Select Police Department',
                      choices = unique(c(crime_data$department_name[which(crime_data$US_State == input$Selected_State)])))
   )
-
-  output$graphs <- renderPlot(grid.arrange(occurance_lineplot(), ratio_plot(), ncol = 2))
+  
+  
+  output$occurance_plot <- renderPlotly(occurance_lineplotly())
+  
+  output$ratio_plot <- renderPlotly(ratio_plotly())
 
   output$State_Crime_Data <- renderTable(summary_table())
 
